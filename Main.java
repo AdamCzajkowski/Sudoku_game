889ecad2727c6941d2daa03e8d45619cad455597
Adam Czajkowski <adam.czajkowski8@gmail.com>
2018-07-14 18:21:35 +0200

    Fixing some bugs

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Main {
     static int sudoku[][] = new int[9][9];
-    static int sudoku1[] = new int[9];
+    static int sudokuSingleLine[] = new int[9];
 
     // Showing sudoku table in 9x9 area
     static void showSudoku() {
@@ -17,13 +17,15 @@ public class Main {
         }
         System.out.println();
     }
-    static void testValue(int i, int j, int w) {
-        System.out.println("i = " + i + ", j = " + j + ", w = " + w);
+
+    static void testValue(int row, int column, int tmp) {
+        System.out.println("row = " + row + ", column = " + column + ", tmp = " + tmp);
         System.out.println();
     }
+
     // Method is printing single line of sudoku.
-    static void showSudoku1() {
-        for (int sudokuSingleElement : sudoku1) {
+    static void showsudokuSingleLine() {
+        for (int sudokuSingleElement : sudokuSingleLine) {
             System.out.print(sudokuSingleElement + " ");
         }
     }
@@ -31,15 +33,15 @@ public class Main {
     // Method is generating in simple way single line of sudoku.
     static void simpleGenerateSingleLineSudoku() {
         Random random = new Random();
-        for (int i = 0; i < 9; i++) {
-            sudoku[0][i] = random.nextInt(9) + 1;
-            for (int j = 1; j <= 9; j++) {
-                if (i == 0) {
+        for (int column = 0; column < 9; column++) {
+            sudoku[0][column] = random.nextInt(9) + 1;
+            for (int row = 1; row <= 9; row++) {
+                if (column == 0) {
                     continue;
                 } else {
-                    if (i >= j) {
-                        if (sudoku[0][i] == sudoku[0][i - j]) {
-                            i--;
+                    if (column >= row) {
+                        if (sudoku[0][column] == sudoku[0][column - row]) {
+                            column--;
                         }
                     }
                 }
@@ -47,89 +49,93 @@ public class Main {
         }
     }
 
-    static void simpleGenerateSudoku() {
+    static void generateSudoku() {
         Random random = new Random();
-        for (int j = 0; j < 9; j++) {
-            for (int i = 0; i < 9; i++) {
-                sudoku[j][i] = random.nextInt(9) + 1;
-                for (int w = 1; w < 9; w++) {
-                    if (i == 0 && j == 0) {
-
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                sudoku[row][column] = random.nextInt(9) + 1;
+                for (int tmp = 1; tmp < 9; tmp++) {
+                    testValue(row,column,tmp);
+                    showSudoku();
+                    if (column == 0 && row == 0) {
                         break;
-                    } else {
-                        if (i > w || j > w) {
-                            showSudoku();
-                            testValue(i,j,w);
-                            if (i == 0) {
-                                if ((sudoku[j][i] == sudoku[j - w][i])) {
-                                    i--;
-                                    break;
-                                }
-                            } else {
-                                if ((sudoku[j][i] == sudoku[j][i - w])) { //|| (sudoku[j][i] == sudoku[j-w][i])) {
-                                    i--;
-                                    break;
-                                }
-                            }
-
+                    }
+                    if (row == 0) {
+                        if (sudoku[row][column] == sudoku[row][column - tmp]) {
+                            column--;
+                            break;
                         }
+                        break;
+                    }
+                    if (column == 0) {
+                        if (sudoku[row][column] == sudoku[row - tmp][column]) {
+                            column--;
+                            break;
+                        }
+                        break;
+                    }
+                    if ((sudoku[row][column] == sudoku[row - tmp][column] && row >= tmp) || ((sudoku[row][column] == sudoku[row][column - tmp] && column >= tmp))) {
+                        column--;
+                        break;
                     }
                 }
             }
         }
     }
+
+
 
 
     // Method is generating single line of sudoku game in step by step.
     static void generateSingleLineSudoku() {
         Random random2 = new Random();
         for (int i = 0; i < 9; i++) {
-            sudoku1[i] = random2.nextInt(9) + 1;
+            sudokuSingleLine[i] = random2.nextInt(9) + 1;
             if (i == 0) {
                 continue;
             }
             if (i == 1) {
-                if (sudoku1[i] == sudoku1[i - 1]) {
+                if (sudokuSingleLine[i] == sudokuSingleLine[i - 1]) {
                     i--;
                 }
             }
             if (i == 2) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2])) {
                     i--;
                 }
             }
             if (i == 3) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3])) {
                     i--;
                 }
             }
             if (i == 4) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3]) || (sudoku1[i] == sudoku1[i - 4])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 4])) {
                     i--;
                 }
             }
             if (i == 5) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3]) || (sudoku1[i] == sudoku1[i - 4]) || (sudoku1[i] == sudoku1[i - 5])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 4]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 5])) {
                     i--;
                 }
             }
             if (i == 6) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3]) || (sudoku1[i] == sudoku1[i - 4]) || (sudoku1[i] == sudoku1[i - 5]) || (sudoku1[i] == sudoku1[i - 6])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 4]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 5]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 6])) {
                     i--;
                 }
             }
             if (i == 7) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3]) || (sudoku1[i] == sudoku1[i - 4]) || (sudoku1[i] == sudoku1[i - 5]) || (sudoku1[i] == sudoku1[i - 6]) || (sudoku1[i] == sudoku1[i - 7])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 4]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 5]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 6]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 7])) {
                     i--;
                 }
             }
             if (i == 8) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3]) || (sudoku1[i] == sudoku1[i - 4]) || (sudoku1[i] == sudoku1[i - 5]) || (sudoku1[i] == sudoku1[i - 6]) || (sudoku1[i] == sudoku1[i - 7]) || (sudoku1[i] == sudoku1[i - 8])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 4]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 5]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 6]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 7]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 8])) {
                     i--;
                 }
             }
             if (i == 9) {
-                if ((sudoku1[i] == sudoku1[i - 1]) || (sudoku1[i] == sudoku1[i - 2]) || (sudoku1[i] == sudoku1[i - 3]) || (sudoku1[i] == sudoku1[i - 4]) || (sudoku1[i] == sudoku1[i - 5]) || (sudoku1[i] == sudoku1[i - 6]) || (sudoku1[i] == sudoku1[i - 7]) || (sudoku1[i] == sudoku1[i - 8]) || (sudoku1[i] == sudoku1[i - 9])) {
+                if ((sudokuSingleLine[i] == sudokuSingleLine[i - 1]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 2]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 3]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 4]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 5]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 6]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 7]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 8]) || (sudokuSingleLine[i] == sudokuSingleLine[i - 9])) {
                     i--;
                 }
             }
@@ -149,7 +155,7 @@ public class Main {
         //showSudoku();
         //generateSingleLineSudoku();
         //simpleGenerateSingleLineSudoku();
-        simpleGenerateSudoku();
+        generateSudoku();
         showSudoku();
 
 
